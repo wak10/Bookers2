19d33b9567ed843ec7f9f6b0f1cbcd2d25c209ea
wak10 <taka.watagashi422@gmail.com>
2021-10-23 06:29:07 +0000

    [add]アソシエーション追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
+  root to: 'homes#top'
+  resources :books, only: [:new, :create, :index, :show, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
