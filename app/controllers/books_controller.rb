733afed482c62829e10e2eb6ee83c2945fdd1a5e
wak10 <taka.watagashi422@gmail.com>
2021-10-25 12:26:40 +0000

    [add]create機能の追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,19 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
   end
 
   def index
+    @book = Book.all
+    @user = current_user
+    @new_book = Book.new
   end
 
   def show
@@ -13,5 +21,11 @@ class BooksController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body, :user_id)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,27 @@
 class UsersController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def index
+    @user = current_user
+  end
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def edit
     # @book = Book.
+  end
+
+  def destroy
+  end
+
+  private
+  def list_params
+    params.require(:user).permit(:name,:introduction, :profile_image_id)
   end
 end
