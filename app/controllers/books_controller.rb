e713cb9e63594f177e321a36c8150e675cd40db8
wak10 <taka.watagashi422@gmail.com>
2021-10-28 12:32:40 +0000

    [add]updeta機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -45,7 +45,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :user_id)
-
+    params.require(:book).permit(:title, :body)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,12 +22,16 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user.id)
+    if @user.update(user_params)
+      flash[:notice]= "You have updated use successfully"
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
   end
 
   private
   def user_params
-    params.require(:user).permit(:name,:introduction, :profile_image_id)
+    params.require(:user).permit(:name,:introduction, :profile_image)
   end
 end
